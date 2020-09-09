77673b04558f3d074211777925d9866ecf4e237c
aabboud1 <abboudahmed1@gmail.com>
2020-09-09 17:52:04 -0400

    added total points functionality to the user class

--- lib/User.rb
+++ lib/User.rb
@@ -12,10 +12,61 @@ class User < ActiveRecord::Base
     def players
         self.teams.map do |team|
             team.players
-        end
+        end.flatten
     end
 
-    # Moveing_players --> change a players team_id to another
+    def team_total_points_stats
+        points_total = 0
+        self.players.map do |player|
+            points_total += player.points
+        end
+        points_total
+    end
+
+    def team_total_rebounds_stats
+        rebound_total = 0
+        self.players.map do |player|
+            rebound_total += player.rebounds
+        end
+        rebound_total
+    end
+
+    def team_total_assist_stats
+        assist_total = 0
+        self.players.map do |player|
+            assist_total += player.assists
+        end
+        assist_total
+    end
+
+    def team_total_steals_stats
+        steal_total = 0
+        self.players.map do |player|
+            steal_total += player.steals
+        end
+        steal_total
+    end
+
+    def all_stats
+        puts "Your team's total points are: #{team_total_points_stats}"
+        puts "Your team's total rebounds are: #{team_total_rebounds_stats}"
+        puts "Your team's total assists are: #{team_total_assist_stats}"
+        puts "Your team's total steals are: #{team_total_steals_stats}"
+    end
+
+    # Moveing_players --> change a players team_id to another 
+    #                     by looking at team_id and reasigning 
+    # def add_players(player)
+    #     if self.players.include?(player)
+    #         puts "You already have this player!"
+    #     else
+    #         self.players.push(player)
+    #         player.all.find do |p|
+    #             p == player
+    #         end.team_id = self.id
+    #     end
+    # end
+
 
 
 end
